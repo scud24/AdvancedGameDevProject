421dd215517338c3c39d6c7e52ae88d1d5c8a12a
ovidiu mocanu <omocanu@uncc.edu>
2019-03-09 22:00:50 -0500

    added deck and hand manager

---
+++ AdvancedGameDevProject/Assets/Scripts/DeckManagerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DeckManagerScript : MonoBehaviour
+{
+
+    private List<GameObject> deck;
+
+
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    //adds a card to the deck
+    void addCard(GameObject card) {
+        deck.Add(card);
+    }
+    //draw the top card of the deck
+    GameObject drawCard() {
+        GameObject card = deck[0];
+        deck.RemoveAt(0);
+        return card;
+    }
+    //shuffles the deck by randomizing the order.
+    void shuffle() {
+        System.Random rng = new System.Random();
+        deck = (List<GameObject>)deck.OrderBy(x => rng.Next());
+    }
+
+}
---
+++ AdvancedGameDevProject/Assets/Scripts/HandManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManagerScript : MonoBehaviour
+{
+    private List<GameObject> hand;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    //used with deck.drawcard to add a card to the hand
+    void drawCard(GameObject card) {
+        hand.Add(card);
+    }
+    //removes a card in the hand at a specific index and returns it.
+    GameObject playCard(int index) {
+        GameObject card = hand[index];
+        deck.RemoveAt(index);
+        return card;
+    }
+
+}
