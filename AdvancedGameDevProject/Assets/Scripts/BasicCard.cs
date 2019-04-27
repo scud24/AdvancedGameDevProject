7946f03e1581a8809469e67ca270441dd00dae99
Nathan Holzworth <thescud24@gmail.com>
2019-04-27 09:50:04 -0400

    Imported Dungeon Player Controller

--- AdvancedGameDevProject/Assets/Scripts/BasicCard.cs
+++ AdvancedGameDevProject/Assets/Scripts/BasicCard.cs
@@ -19,7 +19,6 @@ public class BasicCard : MonoBehaviour
     public GameObject CombatManager;
     public int callBackID;
 
-
     public bool sendsClick = false;
     // Start is called before the first frame update
     void Start()
