ee83c889148d9e70ceaf75562fd64ef7dbf6fe0a
iganbold <itgel.ganbold1@gmail.com>
2019-04-29 14:42:42 -0400

    Implement Map Menu

---
+++ AdvancedGameDevProject/Assets/Scripts/MapMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Dungeon, Inventory, Shop, Save & Quit
+
+    public void Dungeon(string a) {
+        Debug.Log(a);
+    }
+
+    public void Inventory(string a)
+    {
+        Debug.Log(a);
+    }
+
+    public void Shop(string a)
+    {
+        Debug.Log(a);
+    }
+
+    public void SaveQuit(string a)
+    {
+        Debug.Log(a);
+    }
+}
