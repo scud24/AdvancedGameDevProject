0a5db14ff3ae0a9ea2ed8bec9df7d9e34fde007b
ovidiu mocanu <omocanu@uncc.edu>
2019-05-06 00:07:52 -0400

    exported from other branch and added here

---
+++ AdvancedGameDevProject/Assets/Scripts/PackTable.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackTable : MonoBehaviour
+{
+    [SerializeField]
+    private CardsForPacks[] cardPool;
+    //[SerializeField]
+    //private CardData[] cardPack;
+    public GameObject pgm;
+    public PackTable packTable;
+    private List<CardData> playerInv;
+
+
+    void Start() {
+        pgm = GameObject.Find("PersistantGameManager");
+        playerInv = pgm.GetComponent<PersistentGameManager>().playerData.cardInventory;
+    }
+
+
+    public CardData[] rollCardPack() {
+        CardData[] cardPack = new CardData[5];
+        for (int i = 0; i < 5; i++) {
+            int index = Random.Range(8, 23);
+            cardPack[i] = (cardPool[index].Card);
+        }
+        return cardPack;
+    }
+
+    public CardData[] rollSpecialCardPack() {
+        CardData[] cardPack = new CardData[5];
+        int index = Random.Range(0, 8);
+        cardPack[0] = (cardPool[index].Card);
+        for (int i = 1; i < 5; i++) {
+            index = Random.Range(8, 23);
+            cardPack[i] = (cardPool[index].Card);
+        }
+        return cardPack;
+    }
+    public CardData rollCard() {
+        CardData card;
+       int index = Random.Range(8, 23);
+       card = (cardPool[index].Card);
+       return card;
+    }
+
+    public CardData rollSpecialCard() {
+        CardData card;
+        int index = Random.Range(0, 8);
+        card = (cardPool[index].Card);
+        Debug.Log(card);
+        return card;
+    }
+    /*
+    public void addPackToDeck(CardData[] cardPack) {
+        
+        for (int i = 0; i < 5; i++) {
+            playerInv.Add(cardPack[i]);
+        }
+    }
+    
+    public void buyPack() {
+        if (pgm.GetComponent<PersistentGameManager>().playerData.gold >= 50) {
+            pgm.GetComponent<PersistentGameManager>().playerData.gold -= 50;
+            CardData[] pack = rollCardPack();
+            addPackToDeck(pack);
+        }
+        else {
+            Debug.Log("Not enough gold");
+        }
+        
+    }
+    public void buyMultiPack() {
+        if (pgm.GetComponent<PersistentGameManager>().playerData.gold >= 150) {
+            pgm.GetComponent<PersistentGameManager>().playerData.gold -= 150;
+            for (int i = 0; i < 3; i++) {
+                CardData[] pack = rollCardPack();
+                addPackToDeck(pack);
+            }
+        }
+        else {
+            Debug.Log("Not enough gold");
+        }
+
+    }
+
+    public void buySpecialCard(CardData card) {
+        if (pgm.GetComponent<PersistentGameManager>().playerData.gold >= 50) {
+            pgm.GetComponent<PersistentGameManager>().playerData.gold -= 50;
+            playerInv.Add(card);
+        }
+        else {
+            Debug.Log("Not enough gold");
+        }
+
+    }
+
+    public void buySpecialPack() {
+        if (pgm.GetComponent<PersistentGameManager>().playerData.gold >= 50) {
+            pgm.GetComponent<PersistentGameManager>().playerData.gold -= 50;
+            CardData[] pack = rollSpecialCardPack();
+            addPackToDeck(pack);
+        }
+        else {
+            Debug.Log("Not enough gold");
+        }
+    }
+
+
+    */
+
+}
---
+++ AdvancedGameDevProject/Assets/Scripts/ShopMenu.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ShopMenu : MonoBehaviour {
+    public GameObject pgm;
+    public PackTable packTable;
+    private List<CardData> playerInv;
+
+    public GameObject SpecialCard1;
+    public GameObject SpecialCard2;
+    public GameObject SpecialCard3;
+
+
+
+    void Start() {
+        pgm = GameObject.Find("PersistantGameManager");
+        playerInv = pgm.GetComponent<PersistentGameManager>().playerData.cardInventory;
+        resetSpecialCards(); //should move this to PersistantGameManager to only activate on first load and on dungeon clears
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public void addPackToDeck(CardData[] cardPack) {
+
+        for (int i = 0; i < 5; i++) {
+            playerInv.Add(cardPack[i]);
+        }
+    }
+
+    public void buyPack() {
+        if (pgm.GetComponent<PersistentGameManager>().playerData.gold >= 50) {
+            pgm.GetComponent<PersistentGameManager>().playerData.gold -= 50;
+            CardData[] pack = packTable.rollCardPack();
+            addPackToDeck(pack);
+        }
+        else {
+            Debug.Log("Not enough gold");
+        }
+
+    }
+    public void buyMultiPack() {
+        if (pgm.GetComponent<PersistentGameManager>().playerData.gold >= 150) {
+            pgm.GetComponent<PersistentGameManager>().playerData.gold -= 150;
+            for (int i = 0; i < 3; i++) {
+                CardData[] pack = packTable.rollCardPack();
+                addPackToDeck(pack);
+            }
+        }
+        else {
+            Debug.Log("Not enough gold");
+        }
+
+    }
+
+    public void buySpecialCard(BasicCard card) {
+        if (pgm.GetComponent<PersistentGameManager>().playerData.gold >= 50) {
+            pgm.GetComponent<PersistentGameManager>().playerData.gold -= 50;
+            playerInv.Add(card.cardData);
+            card.GetComponentInChildren<Button>().interactable = false;
+        }
+        else {
+            Debug.Log("Not enough gold");
+        }
+
+    }
+
+    public void buySpecialPack() {
+        if (pgm.GetComponent<PersistentGameManager>().playerData.gold >= 50) {
+            pgm.GetComponent<PersistentGameManager>().playerData.gold -= 50;
+            CardData[] pack = packTable.rollSpecialCardPack();
+            addPackToDeck(pack);
+        }
+        else {
+            Debug.Log("Not enough gold");
+        }
+
+
+    }
+    public void resetSpecialCards() {
+        SpecialCard1.GetComponent<BasicCard>().SetCardData(packTable.rollSpecialCard());
+        SpecialCard1.GetComponent<BasicCard>().SetupUI();
+        SpecialCard1.SetActive(true);
+        SpecialCard1.GetComponentInChildren<Button>().interactable = false;
+
+        SpecialCard2.GetComponent<BasicCard>().SetCardData(packTable.rollSpecialCard());
+        SpecialCard2.GetComponent<BasicCard>().SetupUI();
+        SpecialCard2.SetActive(true);
+        SpecialCard1.GetComponentInChildren<Button>().interactable = true;
+
+        SpecialCard3.GetComponent<BasicCard>().SetCardData(packTable.rollSpecialCard());
+        SpecialCard3.GetComponent<BasicCard>().SetupUI();
+        SpecialCard3.SetActive(true);
+        SpecialCard1.GetComponentInChildren<Button>().interactable = true;
+
+    }
+
+    public void exitToMenu() {
+        SceneManager.LoadScene(sceneName: "Map Menu");
+
+    }
+
+
+}
