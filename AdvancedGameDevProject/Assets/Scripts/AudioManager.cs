9b74fc7dc103f80d0be25f0e8f6d67bc7c624068
iganbold <itgel.ganbold1@gmail.com>
2019-05-07 11:11:34 -0400

    Implement audio manager with 35 different sounds

---
+++ AdvancedGameDevProject/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instane;
+    public Sound[] sounds;
+    void Awake()
+    {
+        if(instane == null) {
+            instane = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound sound in sounds) {
+            sound.audioSource = gameObject.AddComponent<AudioSource>();
+            sound.audioSource.clip = sound.clip;
+            sound.audioSource.volume = sound.volume;
+            sound.audioSource.pitch = sound.pitch;
+            sound.audioSource.loop = sound.loop;
+        }
+    }
+
+    public void Play(string name) {
+        Sound resultSound = Array.Find(sounds, sound => sound.name == name);
+        if (resultSound == null) {
+            Debug.LogWarning("Sound: "+name+" not found!");
+        return;
+         }
+
+        resultSound.audioSource.Play();
+    }
+}
---
+++ AdvancedGameDevProject/Assets/Scripts/Sound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+// Mark it as Serializable, so that we can it in inspector
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0f, 1f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+
+    public bool loop; 
+}
